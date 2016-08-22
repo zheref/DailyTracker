e6f1c931b2f6697318eea0a8064e6f980d6cc693
Sergio Daniel Lozano <sergio.lozano@grability.com>
2016-08-22 10:54:51 -0500

    Initial Commit

---
+++ DailyTracker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DailyTracker
+//
+//  Created by Sergio Daniel Lozano on 8/22/16.
+//  Copyright © 2016 zheref. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
